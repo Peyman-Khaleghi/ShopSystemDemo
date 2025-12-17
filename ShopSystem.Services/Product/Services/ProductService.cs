bb455e6699f6690a1bb02785f782a1795ad2e83f
Peymeeeee <Peyman.khaleghi.net@gmail.com>
2025-12-17 10:08:08 +0330

    local commit

--- ShopSystem.Services/Product/Services/ProductService.cs
+++ ShopSystem.Services/Product/Services/ProductService.cs
@@ -7,16 +7,11 @@ namespace ShopSystem.Services;
 //logic for product
 public class ProductService : ApplicationGenericServices<Product, int, ProductItem, ProductOutput, ProductInput, ProductUpdate>, IApplicationServiceAutoRegister
 {
-    private readonly IRepository<Product, int> _repository;
-    private readonly IUnitOfWork _unitOfWork;
-    private readonly IMapper _mapper;
-
     public ProductService(
         IRepository<Product, int> repository,
         IUnitOfWork unitOfWork,
         IMapper mapper)
         : base(repository, unitOfWork, mapper)
     { }
-
 
 }
